8dfcec91ab18b445466e81f1e78a01d8f62c80ed
root <Asmamaw Yismaw>
2022-04-06 01:04:57 -0700

    Then Edit

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-    promise.then(() => {
-	if(success){
-	    resolve({
-	    status: 200,
-	    body: 'success',
-	    });
-	    else {
-      reject(Error('The fake API is not working currently'));
-    }
-	console.log('Got a response from the API');
-  }, (err) => err);
+  const obj = { status: 200, body: 'success' };
+  return promise
+    .then(() => obj)
+    .catch(() => new Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
